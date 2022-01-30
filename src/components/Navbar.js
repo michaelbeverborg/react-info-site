7e771c757077c6b3a3aab97b92642916d7c9fc57
Michael <michaelbeverborg@hotmail.com>
2022-01-30 14:48:42 +0100

    #0

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
